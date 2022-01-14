67ce6980db1c08bc3b684291960751c09c766170
Slava <luidzit@gmail.com>
2022-01-14 23:41:43 +0300

    todo commit

---
+++ src/components/todo-list.js
@@ -0,0 +1,27 @@
+import React from "react";
+import TodoListItem from './todo-list-item'
+
+
+const TodoList = ({ todos }) => {
+
+    const firstEl = (
+        <li>
+            <TodoListItem
+                label={todos[0].label}
+                importan={todos[0].important} />
+        </li>
+    );
+
+    return (
+        <ul>
+            {firstEl}
+            <li>
+                <TodoListItem
+                    label={todos[1].label}
+                    important={todos[1].important} />
+            </li>
+        </ul>
+    );
+};
+
+export default TodoList;
